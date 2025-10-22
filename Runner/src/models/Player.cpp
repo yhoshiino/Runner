9404ae7dcc1d14a5e669969023c242da2366a881
yhoshiino <yhoshiino@gmail.com>
2025-10-22 22:43:09 +0200

    features(Player) : autoMove done. Movement Inputs done.

--- Runner/src/models/Player.cpp
+++ Runner/src/models/Player.cpp
@@ -1,20 +1,34 @@
 #include "Player.h"
 
 
-Player::Player()
+Player::Player(sf::Vector2f spawnPosition, sf::Vector2f initialVelocity)
 {
+
 }
 
 Player::~Player()
 {
+
 }
 
 void Player::update(float deltatime)
 {
+	sf::Vector2f autoMove(0.f, 50.f);
+
+	// normalisation du mouvement manuel
+	if (m_velocity.x != 0.f || m_velocity.y != 0.f)
+	{
+		float len = std::sqrt(m_velocity.x * m_velocity.x + m_velocity.y * m_velocity.y);
+		m_velocity /= len;
+	}
+
+	float manualSpeed = 200.f; // vitesse contrôlée
+	m_position += (m_velocity * manualSpeed + autoMove) * deltatime;
 }
 
 void Player::draw(sf::RenderWindow& window)
 {
+	/*window.draw(sprite);*/
 }
 
 bool Player::isColliding(sf::FloatRect otherHitbox)
@@ -24,8 +38,25 @@ bool Player::isColliding(sf::FloatRect otherHitbox)
 
 void Player::onHit(Entity* otherEntity)
 {
+
 }
 
 void Player::handleInputs()
 {
+	m_velocity = {0.f, 0.f};
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
+	{
+		m_velocity.x = -1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+	{
+		m_velocity.x = 1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z)) {
+		m_velocity.y = -1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+		m_velocity.y = 1;
+	}
 }
